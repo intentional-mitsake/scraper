099ece7d3fc7332702ead545527d5768ee44e274
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2025-10-29 14:15:09 +0545

    added filters & scraped sum

--- main.py
+++ main.py
@@ -9,21 +9,31 @@ api = os.getenv("URL")
 
 current_page = 1
 start = 1
-max_pages = 200
 all_books = []
-file = 'allbooks.json'
+#json file to store the scraped data
+file = 'files/contemporary.json'
+
+#filters
+genreFilter = ["fiction-and-literature", "history-biography-and-social-science"]
+subGenreFilter = ["classics", "fantasy", "science-fiction","mystery-thriller-and-suspense", "historical-fiction", "contemporary"]
+
 
 while True:
-    url = f"{api}?page={current_page}"
+    #for fiction and literature
+    url = f"{api}?genre={genreFilter[0]}&sub_genres={subGenreFilter[5]}&page={current_page}" 
+    #for history
+    #url = f"{api}?genre={genreFilter[1]}&sub_genres=history&page={current_page}"
     print(f"Scraping page {current_page}...")
     req = requests.get(url)
     current_page += 1
     data = req.json()
+    #if there is no more data, break the loop as that means we have reached the end
     if not data['data']:
        print("No more data to scrape.")
        break
-    data = json.dumps(data, indent=4)
+    #data = json.dumps(data, indent=4)
     formatted_data = []
+    #iterate through the data and extract relevant information only
     for book in data['data']:
        name = book['name']
        author = book['authors'] 
@@ -36,8 +46,12 @@ while True:
            'price': price
        })
     try:
+       #with closes the file after writing
        with open(file, 'a', encoding='utf-8') as f:
-           f.write(formatted_data)
+           for item in formatted_data:
+                #to convert dict(item) to string
+                #can only write strings to a file
+                f.write(item.__str__() + '\n')
     except Exception as e:
        print(f"An error occurred: {e}")
         
